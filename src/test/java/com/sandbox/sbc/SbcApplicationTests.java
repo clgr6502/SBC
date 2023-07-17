bae0899385657e02ba83d1aaed9b6587cf143daf
clgr6502 <c.bellavia00@gmail.com>
2023-07-17 21:07:42 +0200

    Initial commit

---
+++ src/test/java/com/sandbox/sbc/SbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandbox.sbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
