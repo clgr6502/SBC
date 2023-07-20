d41dec50f928e2be040608d591eadb6914a1a8fa
clgr6502 <c.bellavia00@gmail.com>
2023-07-20 17:46:25 +0200

    Migliorata la gestione dell'utente non trovato

--- src/main/java/com/sandbox/sbc/services/JwtUserDetailsService.java
+++ src/main/java/com/sandbox/sbc/services/JwtUserDetailsService.java
@@ -2,11 +2,13 @@ package com.sandbox.sbc.services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 
@@ -18,13 +20,16 @@ public class JwtUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        logger.info("Cerco l'utente in base all'username");
+
+        // TODO ricerca nell database
+        // userRepository.findByUsername(username);
 
         if ("admin".equals(username)) {
             return new User("admin", "$2a$12$sZUSIhyVLN7MByX.CzRVf.70b/985QVkGzOJHrF8xavhZpLqd9MfW",
                     new ArrayList<>());
         } else {
-            throw new UsernameNotFoundException("Utente " + username + " non trovato");
+            logger.error("Utente non trovato");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Utente " + username + " non trovato");
         }
     }
 }
