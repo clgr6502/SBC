9a05ee76f2da3d82be6185a7ae320940151ac1d7
clgr6502 <c.bellavia00@gmail.com>
2023-07-20 14:41:24 +0200

    Aggiunto il controller per l'autenticazione del jwt

---
+++ src/main/java/com/sandbox/sbc/controllers/JwtAuthenticationController.java
@@ -0,0 +1,59 @@
+package com.sandbox.sbc.controllers;
+
+import com.sandbox.sbc.requests.JwtRequest;
+import com.sandbox.sbc.responses.JwtResponse;
+import com.sandbox.sbc.services.JwtUserDetailsService;
+import com.sandbox.sbc.utils.JwtTokenUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+public class JwtAuthenticationController {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationController.class);
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private JwtUserDetailsService userDetailsService;
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<JwtResponse> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+
+        logger.info("Invocazione del servizio di autenticazione");
+        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+        final String token = jwtTokenUtil.generateToken(userDetails);
+
+        JwtResponse response = new JwtResponse();
+        response.setUsername(authenticationRequest.getUsername());
+        response.setToken(token);
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
+    }
+
+    private void authenticate(String username, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+    }
+}
