194b852042401e7bc1eddcefd106628392db0f11
clgr6502 <c.bellavia00@gmail.com>
2023-07-20 14:30:29 +0200

    aggiunto log

--- src/main/java/com/sandbox/sbc/services/JwtUserDetailsService.java
+++ src/main/java/com/sandbox/sbc/services/JwtUserDetailsService.java
@@ -1,5 +1,7 @@
 package com.sandbox.sbc.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,10 +13,15 @@ import java.util.ArrayList;
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
 
+    private static final Logger logger = LoggerFactory.getLogger(JwtUserDetailsService.class);
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        logger.info("Cerco l'utente in base all'username");
+        
         if ("admin".equals(username)) {
-            return new User("admin", "root",
+            return new User("admin", "$2a$12$sZUSIhyVLN7MByX.CzRVf.70b/985QVkGzOJHrF8xavhZpLqd9MfW",
                     new ArrayList<>());
         } else {
             throw new UsernameNotFoundException("Utente " + username + " non trovato");
