f3d0dec7c525a9433b2637fb09fe2943829eed9c
clgr6502 <c.bellavia00@gmail.com>
2023-07-21 10:52:26 +0200

    Refactor nome entity e aggiunti getter e setter

---
+++ src/main/java/com/sandbox/sbc/entities/DbUser.java
@@ -0,0 +1,46 @@
+package com.sandbox.sbc.entities;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "users")
+public class DbUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    private String username;
+
+    @NotNull
+    @NotBlank
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- src/main/java/com/sandbox/sbc/repositories/UserRepository.java
+++ src/main/java/com/sandbox/sbc/repositories/UserRepository.java
@@ -1,11 +1,16 @@
 package com.sandbox.sbc.repositories;
 
-import com.sandbox.sbc.entities.User;
+import com.sandbox.sbc.entities.DbUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.Id;
+import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Id> {
+public interface UserRepository extends JpaRepository<DbUser, Id> {
+
+    @Query(nativeQuery = true, value = "SELECT username FROM users WHERE username :=username")
+    Optional<DbUser> findUserByUsername(String username);
 }
