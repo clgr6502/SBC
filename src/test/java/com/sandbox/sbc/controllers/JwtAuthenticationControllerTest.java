ee57cdbe1bbb1f27dfd4896d8fbf081716cc7451
clgr6502 <c.bellavia00@gmail.com>
2023-07-22 18:29:12 +0200

    Aggiunti test di riuscita e fallimento per saveUser

--- src/test/java/com/sandbox/sbc/controllers/JwtAuthenticationControllerTest.java
+++ src/test/java/com/sandbox/sbc/controllers/JwtAuthenticationControllerTest.java
@@ -1,5 +1,8 @@
 package com.sandbox.sbc.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sandbox.sbc.requests.DbUserRequest;
 import com.sandbox.sbc.requests.JwtRequest;
 import com.sandbox.sbc.responses.JwtResponse;
 import com.sandbox.sbc.services.JwtUserDetailsService;
@@ -8,22 +11,33 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Collection;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
+@AutoConfigureMockMvc
 public class JwtAuthenticationControllerTest {
 
     @Autowired
     private JwtAuthenticationController jwtAuthenticationController;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @MockBean
     private AuthenticationManager authenticationManager;
@@ -83,6 +97,47 @@ public class JwtAuthenticationControllerTest {
 
         Assertions.assertThrows(BadCredentialsException.class, () -> jwtAuthenticationController.createAuthenticationToken(request));
 
+    }
+
+    @Test
+    void saveUserShouldBeSuccessful() throws Exception {
+
+        DbUserRequest request = getMockedDbUserRequest();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String req = mapper.writeValueAsString(request);
+
+        Mockito.doNothing().when(jwtUserDetailsService).saveUser(Mockito.any());
+
+        mockMvc.perform(post("/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(req))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void saveUserShouldFailForNullUsername() throws Exception {
+
+        DbUserRequest request = getMockedDbUserRequest();
+        request.setUsername(null);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String req = mapper.writeValueAsString(request);
+
+        mockMvc.perform(post("/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(req))
+                .andExpect(status().isBadRequest());
+    }
+
+    private DbUserRequest getMockedDbUserRequest(){
+
+        DbUserRequest request = new DbUserRequest();
+
+        request.setUsername("mock");
+        request.setPassword("mock");
+
+        return request;
     }
 
     private UserDetails getMockedUserDetail() {
