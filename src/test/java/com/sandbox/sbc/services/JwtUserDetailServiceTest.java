aecdfea92008b8e66db31fe77a098f19e419ea7a
clgr6502 <c.bellavia00@gmail.com>
2023-07-22 18:44:25 +0200

    aggiunti ulteriori test per il controller

--- src/test/java/com/sandbox/sbc/services/JwtUserDetailServiceTest.java
+++ src/test/java/com/sandbox/sbc/services/JwtUserDetailServiceTest.java
@@ -1,8 +1,11 @@
 package com.sandbox.sbc.services;
 
+import com.sandbox.sbc.entities.DbUser;
+import com.sandbox.sbc.repositories.DbUserRepository;
 import com.sandbox.sbc.utils.JwtTokenUtil;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -11,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @SpringBootTest
 public class JwtUserDetailServiceTest {
@@ -19,12 +23,15 @@ public class JwtUserDetailServiceTest {
     private JwtUserDetailsService jwtUserDetailsService;
 
     @MockBean
+    private DbUserRepository dbUserRepository;
+
+    @MockBean
     private JwtTokenUtil jwtTokenUtil;
 
     @Test
-    void loadUserByUsernameShouldBeSuccessful() {
+    void loadUserByUsernameShouldBeSuccessfulForAdminUser() {
 
-        User user = getMockedUser();
+        User user = getAdmin();
         UserDetails userDetails = jwtUserDetailsService.loadUserByUsername("admin");
 
         Assertions.assertEquals(user.getUsername(), userDetails.getUsername());
@@ -32,13 +39,38 @@ public class JwtUserDetailServiceTest {
     }
 
     @Test
+    void loadUserByUsernameShouldBeSuccessfulForUserInDataBase() {
+
+        DbUser dbUser = getMockedDbUser();
+
+        Mockito.when(dbUserRepository.findByUsername(Mockito.any())).thenReturn(Optional.of(dbUser));
+
+        UserDetails userDetails = jwtUserDetailsService.loadUserByUsername("mock");
+
+        Assertions.assertEquals(dbUser.getUsername(), userDetails.getUsername());
+        Assertions.assertEquals(dbUser.getPassword(), userDetails.getPassword());
+    }
+
+    @Test
     void loadUserByUsernameForNonExistentUser() {
+
+        Mockito.when(dbUserRepository.findByUsername(Mockito.any())).thenReturn(Optional.empty());
 
         Assertions.assertThrows(ResponseStatusException.class, () -> jwtUserDetailsService.loadUserByUsername("mock"));
 
     }
 
-    private User getMockedUser() {
+    private User getAdmin() {
         return new User("admin", "$2a$12$sZUSIhyVLN7MByX.CzRVf.70b/985QVkGzOJHrF8xavhZpLqd9MfW", new ArrayList<>());
+    }
+
+    private DbUser getMockedDbUser() {
+
+        DbUser user = new DbUser();
+
+        user.setUsername("mock");
+        user.setPassword("mockPass");
+
+        return user;
     }
 }
