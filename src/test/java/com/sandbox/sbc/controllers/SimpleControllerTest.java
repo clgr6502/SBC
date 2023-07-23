eadfc7741b2a867c5f7511f6e470eb53609644ff
clgr6502 <c.bellavia00@gmail.com>
2023-07-23 09:05:29 +0200

    Aggiunti i test per il nuovo controller con i privilegi

--- src/test/java/com/sandbox/sbc/controllers/SimpleControllerTest.java
+++ src/test/java/com/sandbox/sbc/controllers/SimpleControllerTest.java
@@ -1,5 +1,7 @@
 package com.sandbox.sbc.controllers;
 
+import com.sandbox.sbc.services.JwtUserDetailsService;
+import com.sandbox.sbc.services.SimpleService;
 import com.sandbox.sbc.utils.JwtTokenUtil;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
@@ -12,9 +14,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -28,18 +35,75 @@ public class SimpleControllerTest {
     private SimpleController simpleController;
 
     @MockBean
+    private JwtUserDetailsService jwtUserDetailsService;
+
+    @MockBean
+    private SimpleService simpleService;
+
+    @MockBean
     private JwtTokenUtil jwtTokenUtil;
 
     @Test
     void basicInfosTest() throws Exception {
 
+        User user = getMockedUser("user");
+
         Jws<Claims> claims = new DefaultJws<>(new DefaultJwsHeader(), new DefaultClaims(), "mock");
-        Mockito.when(jwtTokenUtil.getUsernameFromToken(Mockito.any())).thenReturn("admin");
+        Mockito.when(jwtUserDetailsService.loadUserByUsername(Mockito.any())).thenReturn(user);
+        Mockito.when(jwtTokenUtil.getUsernameFromToken(Mockito.any())).thenReturn("user");
         Mockito.when(jwtTokenUtil.validateToken(Mockito.any(), Mockito.any())).thenReturn(true);
         Mockito.when(jwtTokenUtil.generateToken(Mockito.any())).thenReturn(claims.getSignature());
 
         mockMvc.perform(get("/simple")
                         .header("Authorization", "Bearer mock"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void modifyUserAttributeShouldBeSuccessful() throws Exception {
+
+        User user = getMockedUser("admin");
+
+        Jws<Claims> claims = new DefaultJws<>(new DefaultJwsHeader(), new DefaultClaims(), "mock");
+        Mockito.when(jwtUserDetailsService.loadUserByUsername(Mockito.any())).thenReturn(user);
+        Mockito.when(jwtTokenUtil.getUsernameFromToken(Mockito.any())).thenReturn("admin");
+        Mockito.when(jwtTokenUtil.validateToken(Mockito.any(), Mockito.any())).thenReturn(true);
+        Mockito.when(jwtTokenUtil.generateToken(Mockito.any())).thenReturn(claims.getSignature());
+
+        Mockito.doNothing().when(simpleService).modifyUserAttrib(Mockito.any(), Mockito.any());
+
+        mockMvc.perform(put("/simple/flag/{value}/{id}", "t", 1L)
+                        .header("Authorization", "Bearer mock"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void modifyUserAttributeShouldFailForForbiddenAccess() throws Exception {
+
+        User user = getMockedUser("user");
+
+        Jws<Claims> claims = new DefaultJws<>(new DefaultJwsHeader(), new DefaultClaims(), "mock");
+        Mockito.when(jwtUserDetailsService.loadUserByUsername(Mockito.any())).thenReturn(user);
+        Mockito.when(jwtTokenUtil.getUsernameFromToken(Mockito.any())).thenReturn("user");
+        Mockito.when(jwtTokenUtil.validateToken(Mockito.any(), Mockito.any())).thenReturn(true);
+        Mockito.when(jwtTokenUtil.generateToken(Mockito.any())).thenReturn(claims.getSignature());
+
+        Mockito.doNothing().when(simpleService).modifyUserAttrib(Mockito.any(), Mockito.any());
+
+        mockMvc.perform(put("/simple/flag/{value}/{id}", "t", 1L)
+                        .header("Authorization", "Bearer mock"))
+                .andExpect(status().isForbidden());
+    }
+
+    private User getMockedUser(String name) {
+
+        User user;
+
+        if ("admin".equals(name)) {
+            user = new User("admin", "pass", List.of((GrantedAuthority) () -> "admin"));
+        } else {
+            user = new User(name, "pass", List.of((GrantedAuthority) () -> "user"));
+        }
+        return user;
     }
 }
